708566d689033fdf81b119c153f28840f9b052b8
manziro785 <manziro785@gmail.com>
2024-06-19 22:27:54 +0600

    added router and pages

---
+++ src/pages/mainPage/mainPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './mainPage.module.css'
+
+export default function MainPage() {
+  return (
+    <div>M</div>
+  )
+}
---
+++ src/pages/noPage/noPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './noPage.module.css'
+
+export default function NoPage() {
+  return (
+    <div>N</div>
+  )
+}
